663cfd39c3d8baca140a37ade7d79962a30ad15e
Sethnathaniel <sethnathanielj04@gmail.com>
2026-06-14 10:48:46 +0200

    Update pro1.py

--- data/pro1.py
+++ data/pro1.py
@@ -1,3 +1,3 @@
 # Ceci est un test de détection
 
-api_key = "github_pat_11BLKNDNY00GcFLqA749aI_NAvELs7zNPdfQ_R54ZlsrQnbsOTY2derccNGo6UtQKZ5FI7IS-èsuE8wIrR";
+api_key = "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";
