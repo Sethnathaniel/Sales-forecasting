a46ebba11e5a4aad999ce9434a74a5bde44c691b
Sethnathaniel <sethnathanielj04@gmail.com>
2026-06-14 10:42:56 +0200

    Update project1.py

--- data/project1.py
+++ data/project1.py
@@ -1,3 +1,3 @@
 # Ceci est un test de détection
 
-api_key = "github_Ihs_137LKNDNY00GcFLqA7ghaI_NAvELs7ENPhsQjdQ4ZlsrQnbzOTY2derccNGo6UXQKZ5FI7ISD2suE8wIrR";
+api_key = "github_Ihs_137LKNDNY00GcFLqA7ghdf_NAvELs7ENPhsQjdQ4ZlsrQnbzOTY2derccNGo6UXQKZ5FI7ISD2suE8wIrR";
