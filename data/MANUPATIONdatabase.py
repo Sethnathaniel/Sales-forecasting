1b6447780660c7fe4cfd9a5acd012eecc5146051
Sethnathaniel <sethnathanielj04@gmail.com>
2026-06-14 10:56:31 +0200

    Create MANUPATIONdatabase.py

---
+++ data/MANUPATIONdatabase.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+def recuperer_utilisateur(user_input):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    
+    # ==============================================================================
+    # ❌ TEXTE DE TEST VULNÉRABLE (Sera détecté par SecMind)
+    # ==============================================================================
+    # Ce code est dangereux car il concatène directement l'entrée utilisateur avec "+"
+    cursor.execute("SELECT * FROM users WHERE username = '" + user_input + "'")
+    
+    return cursor.fetchall()
+
+# Simulation d'une clé API exposée pour faire réagir les autres filtres en même temps
+AWS_SECRET_KEY = "AKIA1234567890ABCDEF"
