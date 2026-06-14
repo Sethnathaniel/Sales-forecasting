136886925f7420cf8d27254687306348d8447fe6
Sethnathaniel <sethnathanielj04@gmail.com>
2026-06-14 10:44:56 +0200

    Update test.py

--- data/test.py
+++ data/test.py
@@ -1,3 +1,3 @@
 # Ceci est un test de détection
 
-api_key = "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";
+api_key = "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";
