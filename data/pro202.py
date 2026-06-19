6e8aab2ef318a528c600a9bd2a255a9b320ffb6c
Sethnathaniel <sethnathanielj04@gmail.com>
2026-06-19 02:22:46 +0200

    Update pro202.py

--- data/pro202.py
+++ data/pro202.py
@@ -1,3 +1,3 @@
 # Ceci est un test de détectionShelloSSw
 
-api_keyS = "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";
+apis_keyS = "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";
